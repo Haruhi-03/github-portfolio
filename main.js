f46a5986d1425546f25d99fd22528c562636910b
Mocchi <216396597+Haruhi-03@users.noreply.github.com>
2025-09-15 21:11:51 +0800

    Add scroll bar and email function to portfolio

--- main.js
+++ main.js
@@ -34,3 +34,60 @@ const blurHeader = () =>{
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+// Email function
+const contactForm = document.getElementById('contact-form'), 
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {e.preventDefault() 
+
+    // serviceID - template ID - #form - publickey
+    emailjs.sendForm('service_9qv9ixy','template_yj07ti8','#contact-form','TPl3vobBOJSXF_Gwr').then(() =>{
+            // Show message is sent
+            contactMessage.textContent = 'Message sent successfully!'
+
+            // Remove message after five seconds
+            setTimeout(() =>{
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+        }, () =>{
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error)'
+
+        })         
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+// Scroll up
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// Scroll Section Active Link
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
