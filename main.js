3d24baae824f955940bc5c67069c8f47c571d215
Mocchi <216396597+Haruhi-03@users.noreply.github.com>
2025-09-16 12:15:51 +0800

    Add scroll animation and reveal function to portfolio website

--- main.js
+++ main.js
@@ -91,3 +91,17 @@ const scrollActive = () =>{
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .experience, .skills, .contact__container`)
+sr.reveal(`.home__img`, {delay: 600})
+sr.reveal(`.home__scroll`,{delay: 800})
+sr.reveal(`.work__card`, {interval: 100})
+sr.reveal(`.about__content`, {origin: `right`})
+sr.reveal(`.about__img`, {origin: `left`})
